696ee71a73e9437a31a86b17db1a172166a4486d
EnemyArea <enemyarea@makerland.de>
2021-08-04 01:56:18 +0200

    airlock-door

---
+++ SelfSealingAirlocks/SelfSealingAirlocks.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+
+namespace SelfSealingAirlocks
+{
+    // Update sim state setter to make airlock doors gas impermable
+    [HarmonyPatch(typeof(Door), "SetWorldState")]
+    internal class SelfSealingAirlocks_Door_SetWorldState
+    {
+        private static void Postfix(Door __instance)
+        {
+            // If the attached gameobject doesn't exist, exit here
+            if (__instance.gameObject == null) return;
+
+            Door.DoorType doorType = __instance.doorType;
+            if (doorType <= Door.DoorType.ManualPressure || doorType == Door.DoorType.Sealed)
+            {
+                for (var i = 0; i < __instance.building.PlacementCells.Length; i++)
+                {
+                    var offsetCell = __instance.building.PlacementCells[i];
+                    SimMessages.ClearCellProperties(offsetCell, 1);
+                    SimMessages.ClearCellProperties(offsetCell, 2);
+                    SimMessages.ClearCellProperties(offsetCell, 4);
+                    SimMessages.SetCellProperties(offsetCell, (byte)(__instance.CurrentState == Door.ControlState.Auto ? 7 : 4));
+                }
+            }
+        }
+    }
+}
