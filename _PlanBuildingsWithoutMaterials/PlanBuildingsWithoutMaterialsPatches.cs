c5d2ae5c5130e2b30aeabc9521f1b3c918f8a232
EnemyArea <enemyarea@makerland.de>
2021-08-28 22:19:35 +0200

    AlgaeGrower,FlowSplitter,PlanBuildingsWithoutMaterials

---
+++ _PlanBuildingsWithoutMaterials/PlanBuildingsWithoutMaterialsPatches.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace PlanBuildingsWithoutMaterials
+{
+	public class PlanBuildingsWithoutMaterialsPatches
+	{
+		[HarmonyPatch(typeof(MaterialSelector))]
+		[HarmonyPatch(nameof(MaterialSelector.AllowInsufficientMaterialBuild))]
+		public static class MaterialSelector_AllowInsufficientMaterialBuild_Patch
+		{
+			public static bool Prefix(ref bool __result)
+			{
+				__result = true;
+				return false;
+			}
+		}
+	}
+}
