2e715b7e5040e0441f59920ae94bce1c1d4e007e
EnemyArea <enemyarea@makerland.de>
2021-09-11 19:40:24 +0200

    dlc

--- NeonEden/NeonEdenPatch.cs
+++ NeonEden/NeonEdenPatch.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 
 namespace NeonEden
 {
-	// Token: 0x02000003 RID: 3
-	[HarmonyPatch(typeof(Db), "Initialize")]
-	internal class NeonEdenPatch
-	{
-		// Token: 0x06000003 RID: 3 RVA: 0x000020DC File Offset: 0x000002DC
-		public static void Prefix()
-		{
-			string str = "NEON_EDEN";
-			Strings.Add("STRINGS.WORLDS." + str + ".NAME", "Neon Eden");
-			Strings.Add("STRINGS.WORLDS." + str + ".DESCRIPTION", "A huge target location with an abundance of resources.\n\n<smallcaps>Neon Eden will pose almost no challenges and the abundance of resources and space will enable you to support a large colony. </smallcaps>\n\n");
-			string text = "Asteroid_NeonEden";
-			ModUtil.RegisterForTranslation(typeof(NeonEdenPatch));
+    // Token: 0x02000003 RID: 3
+    [HarmonyPatch(typeof(Db), "Initialize")]
+    internal class NeonEdenPatch
+    {
+        // Token: 0x06000003 RID: 3 RVA: 0x000020DC File Offset: 0x000002DC
+        public static void Prefix()
+        {
+            string str = "NEON_EDEN";
+            Strings.Add("STRINGS.WORLDS." + str + ".NAME", "Neon Eden");
+            Strings.Add("STRINGS.WORLDS." + str + ".DESCRIPTION", "A huge target location with an abundance of resources.\n\n<smallcaps>Neon Eden will pose almost no challenges and the abundance of resources and space will enable you to support a large colony. </smallcaps>\n\n");
+            Strings.Add("STRINGS.CLUSTER_NAMES." + str + ".NAME", "Neon Eden");
+            Strings.Add("STRINGS.CLUSTER_NAMES." + str + ".DESCRIPTION", "A huge target location with an abundance of resources.\n\n<smallcaps>Neon Eden will pose almost no challenges and the abundance of resources and space will enable you to support a large colony. </smallcaps>\n\n");
+            Strings.Add("STRINGS.SUBWORLDS.NEONEDEN.NAME", "Neon Eden");
+            Strings.Add("STRINGS.SUBWORLDS.NEONEDEN.DESC", "A huge target location with an abundance of resources.\n\n<smallcaps>Neon Eden will pose almost no challenges and the abundance of resources and space will enable you to support a large colony. </smallcaps>\n\n");
+            Strings.Add("STRINGS.SUBWORLDS.NEONEDEN.UTILITY", "Neon Eden UTILITY");
+            string text = "Asteroid_NeonEden";
+            ModUtil.RegisterForTranslation(typeof(NeonEdenPatch));
 
             var sprite = Assembly.GetExecutingAssembly().GetManifestResourceStream("NeonEden." + text + ".dds");
-
+            Debug.Log(sprite == null ? "sprite is null" : "sprite is not null");
             Sprite value = Sprites.CreateSpriteDXT5(sprite, 512, 512);
             Assets.Sprites.Add(text, value);
-		}
-	}
+
+            var textureBiom = Assembly.GetExecutingAssembly().GetManifestResourceStream("NeonEden.NeonEdenBiom.png");
+            Debug.Log(textureBiom == null ? "textureBiom is null" : "textureBiom is not null");
+            Sprite spriteBiom = Sprites.CreateSpriteFromPng(textureBiom, 512, 512);
+            Assets.Sprites.Add("biomeIconNeoneden", spriteBiom);
+        }
+    }
 }
